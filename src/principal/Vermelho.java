e33f20d991742f94357374328ca1673b84bb2071
Luana Uchoa <uchoa.m.luana@gmail.com>
2017-10-18 22:51:01 -0200

    retira i

--- src/principal/Vermelho.java
+++ src/principal/Vermelho.java
@@ -32,7 +32,6 @@ public class Vermelho extends Thread {
                 //Verifica se processo anterior terminou de executar
                 if( i != 0 ) //condicao para primeira execução
                     this.monitor.get( (posicao)==0?monitor.get_total()-1:(posicao-1) ).acquire();
-                System.out.println("i = " + i);
                 System.out.println("VERMELHO");
                 this.monitor.get( (posicao) ).release();
             }
